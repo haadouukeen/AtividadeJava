8178f5086c6de3edf54ac31e87170d99d7713fb1
Igor Gabriel <gigor818@gmail.com>
2020-11-17 19:56:44 -0300

    first commit

---
+++ src/main/java/com/ativijava/AtivijavaApplication.java
@@ -0,0 +1,13 @@
+package com.ativijava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtivijavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtivijavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ativijava/ativijava/AtivijavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ativijava.ativijava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtivijavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
