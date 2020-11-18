746c744bdf9c64038a7134800496423eff10c7d2
Igor Gabriel <gigor818@gmail.com>
2020-11-17 21:56:59 -0300

    Criar um endpoint utilizando Java + Springboot que receba via parâmetro 2 números e entregue como resposta a soma dos dois.

--- src/main/java/com/ativijava/controllers/IndexController.java
+++ src/main/java/com/ativijava/controllers/IndexController.java
@@ -7,6 +7,8 @@ package com.ativijava.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  *
@@ -18,4 +20,65 @@ public class IndexController {
     public String index(){
         return "index";
     }
+    
+    @RequestMapping(value="/endpoint1",method=RequestMethod.GET)
+    public ModelAndView endpoint1(){
+        ModelAndView mv = new ModelAndView("endpoint1");
+        Vals v  = new Vals();
+        v.setSoma("");
+        mv.addObject("resultado", v);
+        
+        return mv;
+    }
+    
+    @RequestMapping(value="/endpoint1",method=RequestMethod.POST)
+    public ModelAndView endpoint1Post(Vals vals){
+        ModelAndView mv = new ModelAndView("endpoint1");
+        vals.setSoma(String.valueOf(vals.getVal1() + vals.getVal2()));
+        mv.addObject("resultado", vals);
+        
+        return mv;
+    }
+    
+    @RequestMapping("/endpoint2")
+    public String endpoint2(){
+        return "endpoint2";
+    }
+    
+    @RequestMapping("/endpoint3")
+    public String endpoint3(){
+        return "endpoint3";
+    }
+}
+
+class Vals{
+    private int val1;
+    private int val2;
+    private String soma;
+
+    public int getVal1() {
+        return val1;
+    }
+
+    public void setVal1(int val1) {
+        this.val1 = val1;
+    }
+
+    public int getVal2() {
+        return val2;
+    }
+
+    public void setVal2(int val2) {
+        this.val2 = val2;
+    }
+
+    public String getSoma() {
+        return soma;
+    }
+
+    public void setSoma(String soma) {
+        this.soma = soma;
+    }
+    
+    
 }
