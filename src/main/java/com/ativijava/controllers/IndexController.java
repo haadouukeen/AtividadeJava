05be9ac6ff7c500964ae1e703b1abd41f1088440
Igor Gabriel <gigor818@gmail.com>
2021-03-16 18:53:05 -0300

    .

--- src/main/java/com/ativijava/controllers/IndexController.java
+++ src/main/java/com/ativijava/controllers/IndexController.java
@@ -15,7 +15,6 @@ import java.util.ArrayList;
 import java.util.Random;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
